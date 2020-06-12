ee5b1916ba8b2462ada684136bac60c34a6d0535
My Pc <My Pc@DESKTOP-L4QTJKQ>
2020-06-12 09:47:01 +0530

    Updated fcs Ex2

---
+++ MILESTONE 1/src/com/wipro/flowcontrolstatements/Ex2.java
@@ -0,0 +1,13 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex2 
+{
+  public static void main(String[] args)
+  {
+	int a=Integer.parseInt(args[0]);
+	if(a%2==0)
+		System.out.println("Given Integer number is Even");
+	else
+		System.out.println("Given Integer number is Odd");
+  }
+}
