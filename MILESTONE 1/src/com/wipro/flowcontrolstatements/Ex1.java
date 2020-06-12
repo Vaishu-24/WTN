2e3495e0ca39c1cadbc0b975b06e172fbcaedf2d
My Pc <My Pc@DESKTOP-L4QTJKQ>
2020-06-12 09:35:17 +0530

    Updated fcs Ex1

---
+++ MILESTONE 1/src/com/wipro/flowcontrolstatements/Ex1.java
@@ -0,0 +1,16 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex1  
+{
+ public static void main(String[] args)
+   {
+	  int a=Integer.parseInt(args[0]);
+	  if(a<0)
+		  System.out.println("Given Integer number is Negative");
+	  if(a>0)
+		  System.out.println("Given Integer number is Positive");
+	  if(a==0)
+		  System.out.println("Given Integer number is Zero");
+
+   }
+}
