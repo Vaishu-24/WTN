d51c36f9aa15cdc4002f1be5c274dba1e9cdd0d5
My Pc <My Pc@DESKTOP-L4QTJKQ>
2020-06-12 09:11:15 +0530

    Updated Ex3

---
+++ MILESTONE 1/src/com/wipro/laguagebasics/Ex3.java
@@ -0,0 +1,11 @@
+package com.wipro.laguagebasics;
+
+public class Ex3 
+{
+ public static void main(String args[])
+   {
+	 int n=Integer.parseInt(args[0]);
+	 int k=Integer.parseInt(args[1]);
+	 System.out.println(n+k);
+   }
+}
